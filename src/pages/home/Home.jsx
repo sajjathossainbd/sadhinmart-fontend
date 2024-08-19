95f1ccd6c9b3dc358de51b645c612d84036efca8
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-08-19 23:26:49 +0600

    search filed design completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
 import Products from "../../components/Products";
+import Search from "../../components/Search";
+import axios from "axios";
+import { useState } from "react";
+import LoadingSpinner from "../../components/shared/LoadingSpinner";
 
 function Home() {
+  const [searchText, setSearchText] = useState("");
+
+  const { data: products = [], isLoading } = useQuery({
+    queryKey: ["products", searchText],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `https://future-skills-help-center-api.vercel.app/cards?search=${searchText}`
+      );
+      return data?.data;
+    },
+  });
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
   return (
     <div>
+      <Search
+        searchText={searchText}
+        setSearchText={setSearchText}
+        products={products}
+      />
       <Products />
     </div>
   );

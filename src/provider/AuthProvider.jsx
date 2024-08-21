e211a74513d4f918ab8e96374c562495201653cc
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-08-22 00:51:21 +0600

    firebase api key update

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -53,12 +53,6 @@ const AuthProvider = ({ children }) => {
       if (currentUser) {
         // get token and store client
         const userInfo = { email: currentUser.email };
-        axiosPublic.post("/jwt", userInfo).then((res) => {
-          if (res.data.token) {
-            localStorage.setItem("access-token", res.data.token);
-            setLoading(false);
-          }
-        });
       } else {
         localStorage.removeItem("access-token");
         setLoading(false);
